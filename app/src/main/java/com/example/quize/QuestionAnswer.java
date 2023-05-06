9de55e43611d395137481f199555fd4f705c54e0
ADMIN <chamodmadusanka012@gmail.com>
2023-05-06 19:42:41 +0530

    Final Commit

---
+++ app/src/main/java/com/example/quize/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.quize;
+
+public class QuestionAnswer {
+
+    public static  String question[] ={
+            "Which company owns the Android?",
+            "Which one is note Programming language",
+            "what is the most famouse social media?"
+    };
+
+    public static  String choices[][] = {
+            {"Google","apple","Nokia","Samsung"},
+            {"java","kotlin","notepad","Python"},
+            {"Facebook","whatsapp","instagram","youtube"}
+    };
+
+    public static  String correctAnswers[] = {
+            "Google",
+            "notepad",
+            "Facebook"
+    };
+
+
+}
